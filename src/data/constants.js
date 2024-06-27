2b6fca5f0951f6254d9b213ebadccb029b543d9b
ARAVIND KUMAR <142021007+Aravind-Kumar1@users.noreply.github.com>
2024-06-27 12:31:17 +0530

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,244 @@
+import image from '../images/Screenshot 2023-11-23 001133.png'
+import im from '../images/campusbuzz.png'
+import i from '../images/Screenshot 2023-11-23 185214.png'
+import img1 from '../images/img.jpg'
+import express from '../images/express.png'
+import codegnan from '../images/codegnan.png'
+import peters from '../images/peters.png'
+import sri from '../images/sri.jpeg'
+
+
+export const Bio = {
+  name: "Aravind Chamaakuri",
+  roles: [
+    "Software Engineer",
+    "Full Stack Developer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Aravind-Kumar1",
+  resume:
+    "https://drive.google.com/file/d/1AvzQBo5ubLPkKMxgh7Q9XkMf8EUumiZP/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/chamaakuri-aravind/",
+  insta: "https://www.instagram.com/aravind_paul01/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+    
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    
+      
+    ],
+  },
+  // {
+  //   title: "Backend",
+  //   skills: [
+  //     {
+  //       name: "Node Js",
+  //       image: "https://nodejs.org/static/images/logo.svg",
+  //     },
+  //     {
+  //       name: "Express Js",
+  //       image:
+  //         express,
+  //     },
+      
+  //     {
+  //       name: "Python",
+  //       image:
+  //         "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+  //     },
+  //     {
+  //       name: "MySQL",
+  //       image:
+  //         "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+  //     },
+  //     {
+  //       name: "Postgresql",
+  //       image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+  //     },
+  //     {
+  //       name: "MongoDB",
+  //       image:
+  //         "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+  //     },
+   
+  //   ],
+  // },
+
+  {
+    title: "Others",
+    skills: [
+      // {
+      //   name: "Git",
+      //   image:
+      //     "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      // },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+   
+     
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      // {
+      //   name: "Postman",
+      //   image:
+      //     "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      // },
+     
+    
+    ],
+  },
+];
+
+export const experiences = [
+ 
+  {
+    id: 4,
+    role: "Web Developer",
+    project: "Flash Feed News Web App",
+    desc: "Created a news web application using HTML, CSS, and JavaScript, integrating a news API to display the latest news.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript"
+    ]
+  },
+  {
+    id: 5,
+    role: "Mobile App Developer",
+    project: "CampusBuzz",
+    date: "Aug 2023 - Jan 2024",
+    desc: "Developed a mobile app as a one-stop solution for event discovery, compatible with both Android and iOS platforms.",
+    skills: [
+      "Flutter",
+      "Dart",
+      "GetX",
+      "Firebase"
+    ],
+    achievements: [
+      "Anticipated a 40% increase in event attendance and a 60% boost in event reach post-launch."
+    ]
+  }
+];
+
+
+export const education = [
+  {
+    id: 0,
+    img:peters,
+    school: "St.Peter's Engineering College,Hyderabad",
+    date: "Oct 2021 - Sep 2025",
+    grade: "83.2 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering atSt.Peter's Engineering College,Hyderabad. I have completed 6 semesters and have a CGPA of 83.2. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor in Engineering - BE, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: sri,
+    school: "Sri Chaitanya Junior College, Hyderabad",
+    date: "Apr 2019 - Apr 2021",
+    grade: "87.4%",
+    desc: "I completed my Intermediate at Sri Chaitanya Junior College,Hyderabad, where I studied MPC(Maths,Physics,Chemistry).",
+    degree: "TSBIE(XII), MPC",
+  },
+  
+];
+
+export const projects = [
+  {
+    id: 4,
+    title: "Flash Feed News Aggregator",
+    date: "Jun 2023 - Aug 2023",
+    description:
+      "Developed a dynamic news web application using HTML, CSS, and JavaScript, incorporating a news API to provide users with a curated collection of articles from various categories. Implemented social media sharing functionality to allow users to share articles seamlessly across their networks. The app ensures users have access to up-to-date information in one convenient platform, enhancing their ability to stay informed and engaged"
+,
+    image:i,
+    tags: [
+      
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "News API"
+    ],
+    category: "web app",
+    github: "https://github.com/Aravind-Kumar1/FLASHFEED-NEWS-WEB-APP",
+    webapp: "https://github.com/Aravind-Kumar1/FLASHFEED-NEWS-WEB-APP",
+  },
+  
+  {
+    id: 9,
+    title: "Campus Buzz Web-App",
+    date: "Sep 2023",
+    description:
+      "Developed a web application designed as a comprehensive solution for event discovery, tailored for seamless compatibility with both Android and iOS platforms. The app empowers users to effortlessly explore a wide range of events, from social gatherings to professional conferences, through intuitive navigation and personalized recommendations. Utilized technologies such as Flutter and Dart for frontend development, ensuring a smooth user experience across platforms. Firebase was employed for backend operations, facilitating robust performance and real-time updates. Anticipated outcomes include a projected 40% increase in event attendance and a 60% boost in event reach post-launch, highlighting its role in enhancing event discovery and fostering community engagement."
+,
+    image:image,
+    tags: [   "Flutter",
+      "Dart",
+      "GetX",
+      "Firebase"],
+    category: "web app",
+    github: "https://github.com/Aravind-Kumar1/flutter-admin-portal",
+    webapp: "https://github.com/Aravind-Kumar1/flutter-admin-portal",
+  },
+  {
+    id: 9,
+    title: "Campus Buzz Mobile-App",
+    date: "Sep 2023",
+    description:
+      "Developed a mobile application designed as a comprehensive solution for event discovery, tailored for seamless compatibility with both Android and iOS platforms. The app empowers users to effortlessly explore a wide range of events, from social gatherings to professional conferences, through intuitive navigation and personalized recommendations. Leveraged technologies such as Flutter, Dart, GetX for state management, and Firebase for backend operations to ensure robust performance and real-time updates. Anticipated outcomes include a projected 40% increase in event attendance and a 60% boost in event reach post-launch, underscoring its effectiveness in enhancing event discovery and community engagement."
+,
+    image:im,
+    tags: [   "Flutter",
+      "Dart",
+      "Firebase"],
+    category: "machine learning",
+    github: "https://github.com/TheMakers404/campusbuzz",
+    webapp: "https://github.com/TheMakers404/campusbuzz",
+  },
+
+
+
+
+
+];
+
+
